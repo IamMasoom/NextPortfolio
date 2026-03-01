d43801082f70883105ecd869e117b8de9b7f64a9
Johan <iam.masoom92@gmail.com>
2026-03-01 15:31:31 +0530

    skipper added

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import LandingPage from "../mainUi/LandingPage"
+
+
+const page = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  )
+}
+
+export default page
---
+++ src/mainUi/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import { CrowdCanvas } from "@/components/ui/skiper-ui/skiper39"
+
+const HeroSection = () => {
+  return (
+    <div>
+      <CrowdCanvas src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/open-peeps-sheet.png' rows={15} cols={7} />
+    </div>
+  )
+}
+
+export default HeroSection
